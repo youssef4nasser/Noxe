3df77084489d767c9fb0d597e998a465aefd6914
youssef4nasser <youssef4nasser@gmail.com>
2023-07-24 10:32:36 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const basename = process.env.NODE_ENV === 'production' ? '/Noxe' : '/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename={basename}>
