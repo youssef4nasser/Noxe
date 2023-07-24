e9af7ee8e5ca3410f6e849deb669f1d8be9f0b20
youssef4nasser <youssef4nasser@gmail.com>
2023-07-24 10:22:02 +0300

    update url home page

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
+const basename = process.env.NODE_ENV === 'production' ? '/Noxe' : '/';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <BrowserRouter basename={basename}>
   <App/>
-
+  </BrowserRouter>,
 );
